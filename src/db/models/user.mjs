7b8b620cc49d9a1e0824e303f8822b70aef08acf
Jose David Duque Gutierrez <31631317+jdduque02@users.noreply.github.com>
2023-09-11 22:05:40 -0500

    feat: :sparkles: implementing user data models
    implementing user data models with their crud base and implementing password encryption

--- src/db/models/user.mjs
+++ src/db/models/user.mjs
@@ -1,12 +1,86 @@
-import userSchema from '../schemas/user'
+import User from '../schemas/user'
+import { bcrypt, HASH_KEY_USER, dateFns, TIMEZONE } from '../modules.mjs'
 export class userModel {
+    // es una función asincrónica estática que recupera todos los usuarios de la base de datos según los parámetros proporcionados.
     static async getAllUsers({ parameters }) {
+        if (!parameters) return false;
         let usersDBFind;
         try {
-            usersDBFind = await userSchema.find(parameters);
+            usersDBFind = await User.find(parameters);
         } catch (error) {
             throw new Error(error.message);
         }
         return usersDBFind
+    }
+    //es una función asincrónica que recupera un usuario de la base de datos según el `id` proporcionado.
+    static async getByIdUser({ id }) {
+        if (!id) return false;
+        let usersDBFindOne;
+        try {
+            usersDBFindOne = await User.findOne(id);
+        } catch (error) {
+            return error;
+        }
+        return usersDBFindOne;
+    }
+    //La función es responsable de crear un nuevo usuario en la base de datos.
+    static async createUser(body) {
+        let salt;
+        try {
+            salt = await bcrypt.genSalt(15, HASH_KEY_USER);
+        } catch (error) {
+            return error
+        }
+        let hash;
+        try {
+            hash = await bcrypt.hash(body.password, salt);
+        } catch (error) {
+            return error;
+        }
+        body.password = hash;
+        let newUser;
+        try {
+            newUser = new User(body);
+        } catch (error) {
+            return error;
+        }
+        let saveNewUser;
+        try {
+            saveNewUser = await newUser.save();
+        } catch (error) {
+            return error;
+        }
+        return saveNewUser;
+    }
+    //La función `deleteUser` es una función asíncrona estática que elimina un usuario de la base de datos según el `userDB` proporcionado.
+    static async deleteUser({ userDB }) {
+        if (!userDB) return false;
+        let userId = userDB._id;
+        let userDBDelete;
+        try {
+            // eslint-disable-next-line no-unused-vars
+            userDBDelete = await User.deleteOne({ _id: userId })
+        } catch (error) {
+            return error;
+        }
+        return true;
+    }
+    //La función `updateUser` es responsable de actualizar un usuario en la base de datos según los parámetros `userDB` y `input` proporcionados.
+    static async updateUser({ userDB, input }) {
+        if (!userDB) return false;
+        const today = new Date();
+        dateFns.setZone(today, TIMEZONE);
+        input.update_at = today;
+        const updateUser = {
+            ...userDB,
+            ...input,
+        };
+        let saveUpdateUser;
+        try {
+            saveUpdateUser = await updateUser.save();
+        } catch (error) {
+            return error;
+        }
+        return saveUpdateUser;
     }
 }
--- src/db/modules.mjs
+++ src/db/modules.mjs
@@ -2,13 +2,16 @@ import { Schema, model } from 'moongose';
 import * as dateFns from "date-fns";
 import dotenv from 'dotenv';
 import { pathEnv } from '../middleware/dontenv.mjs';
+import bcrypt from 'bcrypt';
 let env = dotenv.config({ path: pathEnv });
 env = env.parsed;
-const { TIMEZONE } = env;
+const { TIMEZONE, HASH_KEY_USER } = env;
 
 export default {
     Schema,
+    HASH_KEY_USER,
     model,
+    bcrypt,
     dateFns,
     TIMEZONE
 };
