e0c05949d59c0b1a104198d5225a06aa78bc0aa3
jdduque02 <jdduquegutierrez02@gmail.com>
2023-09-21 17:08:28 -0500

    fix: :test_tube: error correction when running tests

--- src/tests/modules.mjs
+++ src/tests/modules.mjs
@@ -2,10 +2,12 @@
 import dotenv from 'dotenv';
 import app from '../app.mjs';
 import { pathEnv } from '../middleware/dontenv.mjs';
+import request from 'supertest';
 let env = dotenv.config({ path: pathEnv });
 env = env.parsed;
 export const { VERSION } = env;
 export default {
     VERSION,
-    app
+    app,
+    request
 }
--- src/tests/user/create.test.mjs
+++ src/tests/user/create.test.mjs
@@ -1,6 +1,6 @@
 /* import * as modules from '../modules.mjs';
 
-const {app,  VERSION } = modules;
+const {app,  VERSION, request } = modules;
 
 const BASEURL = `/api/v${VERSION}`;
 const routeCreateUser = `${BASEURL}/user/create/`;
@@ -13,8 +13,8 @@ describe(`POST ${routeCreateUser}`, () => {
         role:1,
     };
     test('to be answered by the state 200', async () => {
-        const request = await request(app).post(`POST ${routeCreateUser}`).send(userCreate);
-        expect(request.statusCode).toBe(200);
+        const queryRequest = await request(app).post(`POST ${routeCreateUser}`).send(userCreate);
+        expect(queryRequest.statusCode).toBe(200);
     })
 })
  */
